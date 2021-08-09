62cef6e44853a0e386633cf14832ac8d0a92b564
VarunDevPro <28918411+VarunDevPro@users.noreply.github.com>
2021-08-09 08:03:46 +0530

    Add support for no-lang code blocks

--- src/CodeBlock.js
+++ src/CodeBlock.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import Prism from 'prism-react-renderer/prism';
-import Highlight, {defaultProps} from 'prism-react-renderer'
-import dartLang from 'refractor/lang/dart';
+import React from "react";
+import Prism from "prism-react-renderer/prism";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import dartLang from "refractor/lang/dart";
 dartLang(Prism);
 
-export default function CodeBlock ({children, className}) {
-  const language = className.replace(/language-/, '')
+export default function CodeBlock({ children, className }) {
+  const language = className ? className.replace(/language-/, "") : "";
 
   return (
-    <Highlight {...defaultProps} Prism={Prism} code={children} language={language}>
-      {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <pre className={className} style={{...style, padding: '20px'}}>
+    <Highlight
+      {...defaultProps}
+      Prism={Prism}
+      code={children}
+      language={language}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: "20px" }}>
           {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({line, key: i})}>
+            <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
-                <span key={key} {...getTokenProps({token, key})} style={{}} />
+                <span key={key} {...getTokenProps({ token, key })} style={{}} />
               ))}
             </div>
           ))}
         </pre>
       )}
     </Highlight>
-  )
+  );
 }
